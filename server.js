14bc9dbde4c276350ed70d00be6aff7cec02d93a
shintaronishimura <md.sakura0803@gmail.com>
2023-07-22 15:51:12 +0900

    MongoDB接続とpostmanによるcrudの追加

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("nodejs");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const foodRouter = require("./routes/foodRoutes");
+
+app.use(foodRouter);
+
+mongoose
+  .connect(
+    "mongodb+srv://shintaro:0203Peko@cluster0.sbonyye.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("データベース接続に成功しました。"))
+  .catch((err) => console.log(err));
+
+app.listen(3000, () => {
+  console.log("サーバーが起動しました。");
+});
